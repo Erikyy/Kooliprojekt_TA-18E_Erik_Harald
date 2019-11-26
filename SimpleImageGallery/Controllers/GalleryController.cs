ef8cf997c08bc2aff200425c02e9adc6d539ae9f
opilane <opilane@KLASSA108-15>
2019-11-26 13:54:44 +0200

    Initial commit, create Data project

---
+++ SimpleImageGallery/Controllers/GalleryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleImageGallery.Models;
+
+namespace SimpleImageGallery.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new GalleryIndexModel()
+            {
+
+            };
+
+            return View();
+        }
+    }
+}
