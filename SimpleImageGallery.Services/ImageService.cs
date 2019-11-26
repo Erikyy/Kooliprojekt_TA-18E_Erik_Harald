e733116a89775618e35f7027ee989e4346f6b90b
opilane <opilane@KLASSA108-15>
2019-11-26 15:49:36 +0200

    Do stuff

---
+++ SimpleImageGallery.Services/ImageService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleImageGallery.Data;
+using SimpleImageGallery.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleImageGallery.Services
+{
+    public class ImageService : IImage
+    {
+        private readonly SimpleImageGalleryDbContext _ctx;
+        public ImageService(SimpleImageGalleryDbContext ctx) 
+        {
+            _ctx = ctx;
+        }
+        public IEnumerable<GalleryImage> GetAll()
+        {
+            return _ctx.GalleryImages
+                .Include(img => img.Tags);
+        }
+
+        public GalleryImage GetById(int id)
+        {
+            return _ctx.GalleryImages.Find(id);
+        }
+        public IEnumerable<GalleryImage> GetWithTag(string tag)
+        {
+            return GetAll().Where(img
+                => img.Tags
+                .Any(t => t.Description == tag));
+        }
+    }
+}
--- SimpleImageGallery/Controllers/GalleryController.cs
+++ SimpleImageGallery/Controllers/GalleryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SimpleImageGallery.Data;
 using SimpleImageGallery.Data.Models;
 using SimpleImageGallery.Models;
 
@@ -10,58 +11,16 @@ namespace SimpleImageGallery.Controllers
 {
     public class GalleryController : Controller
     {
+        private readonly IImage _imageService;
+        public GalleryController(IImage imageService) 
+        {
+            _imageService = imageService;
+        }
+
         public IActionResult Index()
         {
-            var hikingImageTags = new List<ImageTag>();
-            var cityImageTags = new List<ImageTag>();
+            var imageList = _imageService.GetAll();
 
-            var tag1 = new ImageTag()
-            {
-                Description = "Adventure",
-                Id = 0
-            };
-
-            var tag2 = new ImageTag()
-            {
-                Description = "Urban",
-                Id = 1
-            };
-
-            var tag3 = new ImageTag()
-            {
-                Description = "New York",
-                Id = 2
-            };
-
-            hikingImageTags.Add(tag1);
-            cityImageTags.AddRange(new List<ImageTag> { tag2, tag3 });
-
-            var imageList = new List<GalleryImage>()
-            {
-                new GalleryImage()
-                {
-                    Title = "Hiking Trip",
-                    Url = "https://images.pexels.com/photos/372098/pexels-photo-372098.jpeg",
-                    Created = DateTime.Now,
-                    Tags = hikingImageTags
-                },
-
-                new GalleryImage()
-                {
-                    Title = "On The Trail",
-                    Url = "https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg",
-                    Created = DateTime.Now,
-                    Tags = hikingImageTags
-                },
-
-                new GalleryImage()
-                {
-                    Title = "Downtown",
-                    Url = "https://images.pexels.com/photos/1034662/pexels-photo-1034662.jpeg",
-                    Created = DateTime.Now,
-                    Tags = cityImageTags
-                }
-            };
             var model = new GalleryIndexModel()
             {
                 Images = imageList,
