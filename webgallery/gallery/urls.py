63daaa7d4adf78f22425668696291f8fbbebb2ef
Erikyy <erik.ylavere@tptlive.ee>
2020-09-08 15:55:05 +0300

    Update project

--- webgallery/gallery/urls.py
+++ webgallery/gallery/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from gallery import views
 
 urlpatterns = [
-    path('', views.gallery, name="gallery")
+    path('', views.gallery, name="gallery"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
 ]
--- webgallery/gallery/views.py
+++ webgallery/gallery/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
 def gallery(request):
+    context = { 'page_title': 'Main Page'}
     return render(request, 'pages/main.html', {})
+
+
+def login(request):
+    context = { 'page_title': 'Login'}
+    return render(request, 'pages/login.html', {})
+
+def register(request):
+    context = { 'page_title': 'Register'}
+    return render(request, 'pages/register.html', {})
