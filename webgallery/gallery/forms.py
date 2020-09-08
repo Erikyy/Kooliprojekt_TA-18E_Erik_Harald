4817eaf179d285d2d34c3b2fcc42b2b51f4c126e
Havardox <harald.varner@gmail.com>
2020-09-08 21:41:32 +0300

    Improve register and login form design

--- webgallery/gallery/forms.py
+++ webgallery/gallery/forms.py
@@ -4,10 +4,22 @@ from django.contrib.auth.models import User
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control'}
+    ))
+
+    password = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control'}
+    ))
+
+    email = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control'}
+    ))
+
     class Meta:
         model = User
         fields = ('username', 'password', 'email')
+
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
